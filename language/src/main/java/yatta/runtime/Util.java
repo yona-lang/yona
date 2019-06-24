03c0e1cdc49cfdc9a2c1b0a09b458f7b694a1c3e
kurobako <wyyyrd@gmail.com>
2019-06-24 17:25:03 +0200

    utils

--- language/src/main/java/yatta/runtime/Util.java
+++ language/src/main/java/yatta/runtime/Util.java
@@ -1,35 +1,61 @@
 package yatta.runtime;
 
-final class Util {
+public final class Util {
   private Util() {}
 
-  static int varIntLen(int value) {
-    for (int i = 1; i < 5; i++) {
-      if (((0xffffffff << (7 * i)) & value) == 0) return i;
-    }
-    return 5;
+  public static short int16Read(final byte[] src, int offset) {
+    return (short) ((src[offset] << 8) | (src[++offset] & 0xff));
   }
 
-  static void varIntWrite(int value, byte[] destination, int offset) {
-    while ((0xffffff80 & value) != 0) {
+  public static void int16Write(final short val, final byte[] dst, int offset) {
+    dst[offset] = (byte) (val >> 8);
+    dst[++offset] = (byte) val;
+  }
+
+  public static int int32Read(final byte[] src, int offset) {
+    return (src[offset] << 24) | ((src[++offset] & 0xff) << 16) | ((src[++offset] & 0xff) <<  8) | (src[++offset] & 0xff);
+  }
+
+  public static void int32Write(int value, final byte[] destination, int offset) {
+    destination[offset] = (byte)(value >> 24);
+    destination[++offset] = (byte)(value >> 16);
+    destination[++offset] = (byte)(value >> 8);
+    destination[++offset] = (byte) value;
+  }
+
+  public static long varInt63Read(final byte[] source, int offset) {
+    long result = 0;
+    byte piece;
+    for (int shift = 0; shift <= 63; shift += 7) {
+      piece = source[offset++];
+      result |= (0x7fL & piece) << shift;
+      if ((0x80 & piece) == 0) break;
+    }
+    return result;
+  }
+
+  public static int varInt63Len(long value) {
+    int result = 1;
+    while ((0xffffffffffffff80L & value) != 0) {
+      result++;
+      value >>>= 7;
+    }
+    return result;
+  }
+
+  public static void varInt63Write(long value, byte[] destination, int offset) {
+    while ((0xffffffffffffff80L & value) != 0) {
       destination[offset++] = (byte) ((0x7f & value) | 0x80);
       value >>>= 7;
     }
     destination[offset] = (byte) value;
   }
 
-  static int varIntRead(byte[] source, int offset) {
-    int result = 0;
-    byte piece;
-    for (int shift = 0; shift <= 28; shift += 7) {
-      piece = source[offset++];
-      result |= (0x7f & piece) << shift;
-      if ((0x80 & piece) == 0) break;
-    }
-    return result;
+  public static boolean nthBit(final short value, final int n) {
+    return ((value << n) & 0x8000) != 0;
   }
 
-  static int codePointAt(byte[] array, int offset) {
+  public static int codePointAt(byte[] array, int offset) {
     final byte b0 = array[offset++];
     if ((0x80 & b0) == 0) return b0;
     final byte b1 = array[offset++];
@@ -40,11 +66,60 @@ final class Util {
     return ((0x7 & b0) << 18) | ((0x3f & b1) << 12) | ((0x3f & b2) << 6) | (0x3f & b3);
   }
 
-  static int codePointLen(int codePoint) {
+  public static int codePointLen(int codePoint) {
     if (codePoint < 0x80) return 1;
     if (codePoint < 0x800) return 2;
     if (codePoint < 0x10000) return 3;
     if (codePoint < 0x10ffff) return 4;
     throw new AssertionError();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  public static int varIntLen(int value) {
+    for (int i = 1; i < 5; i++) {
+      if (((0xffffffff << (7 * i)) & value) == 0) return i;
+    }
+    return 5;
+  }
+
+  public static void varIntWrite(int value, byte[] destination, int offset) {
+    while ((0xffffff80 & value) != 0) {
+      destination[offset++] = (byte) ((0x7f & value) | 0x80);
+      value >>>= 7;
+    }
+    destination[offset] = (byte) value;
+  }
+
+  public static int varIntRead(byte[] source, int offset) {
+    int result = 0;
+    byte piece;
+    for (int shift = 0; shift <= 28; shift += 7) {
+      piece = source[offset++];
+      result |= (0x7f & piece) << shift;
+      if ((0x80 & piece) == 0) break;
+    }
+    return result;
   }
 }
--- language/src/test/java/yatta/runtime/UtilTest.java
+++ language/src/test/java/yatta/runtime/UtilTest.java
@@ -9,35 +9,103 @@ import static yatta.runtime.Util.*;
 public class UtilTest {
 
   @Test
-  public void testVarIntReadWrite() {
-    testVarIntReadWrite(new byte[1], 0x0);
-    testVarIntReadWrite(new byte[1], 0x7a);
-    testVarIntReadWrite(new byte[2], 0x8a);
-    testVarIntReadWrite(new byte[2], 0x3abc);
-    testVarIntReadWrite(new byte[3], 0x4abc);
-    testVarIntReadWrite(new byte[3], 0x1abcde);
-    testVarIntReadWrite(new byte[4], 0x2abcde);
-    testVarIntReadWrite(new byte[4], 0xfabcdef);
-    testVarIntReadWrite(new byte[5], 0x1abcdef0);
-    testVarIntReadWrite(new byte[5], 0x7fffffff);
-  }
-
-  private static void testVarIntReadWrite(byte[] bytes, int value) {
-    varIntWrite(value, bytes, 0);
-    assertEquals(value, varIntRead(bytes, 0));
+  public void testInt16ReadWrite() {
+    final byte[] data = new byte[2];
+    Util.int16Write((short) 0, data, 0);
+    assertEquals((short) 0, Util.int16Read(data, 0));
+    final short[] vals = new short[]{
+     0x1, 0x2, 0x4, 0x8,
+     0x1a, 0x2a, 0x4a, 0x8a,
+     0x1ab, 0x2ab, 0x4ab, 0x8ab,
+     0x1abc, 0x2abc, 0x4abc, (short) 0x8abc
+    };
+    for (short val : vals) {
+      Util.int16Write(val, data, 0);
+      assertEquals(val, Util.int16Read(data, 0));
+    }
   }
 
   @Test
-  public void testVarIntLen() {
-    assertEquals(1, varIntLen(0x0));
-    assertEquals(1, varIntLen(0x7a));
-    assertEquals(2, varIntLen(0x8a));
-    assertEquals(2, varIntLen(0x3abc));
-    assertEquals(3, varIntLen(0x4abc));
-    assertEquals(3, varIntLen(0x1abcde));
-    assertEquals(4, varIntLen(0x2abcde));
-    assertEquals(4, varIntLen(0xfabcdef));
-    assertEquals(5, varIntLen(0x1abcdef0));
-    assertEquals(5, varIntLen(0x7fffffff));
+  public void testInt32ReadWrite() {
+    final byte[] data = new byte[4];
+    Util.int32Write(0, data, 0);
+    assertEquals(0, Util.int32Read(data, 0));
+    final int[] vals = new int[]{
+     0x1, 0x2, 0x4, 0x8,
+     0x1a, 0x2a, 0x4a, 0x8a,
+     0x1ab, 0x2ab, 0x4ab, 0x8ab,
+     0x1abc, 0x2abc, 0x4abc, 0x8abc,
+     0x1abcd, 0x2abcd, 0x4abcd, 0x8abcd,
+     0x1abcde, 0x2abcde, 0x4abcde, 0x8abcde,
+     0x1abcdef, 0x2abcdef, 0x4abcdef, 0x8abcdef,
+     0x1abcdef0, 0x2abcdef0, 0x4abcdef0, 0x8abcdef0
+    };
+    for (int val : vals) {
+      Util.int32Write(val, data, 0);
+      assertEquals(val, Util.int32Read(data, 0));
+    }
+  }
+
+  @Test
+  public void testVarInt63Len() {
+    assertEquals(1, Util.varInt63Len(0x0L));
+    assertEquals(1, Util.varInt63Len(0x7aL));
+    assertEquals(2, Util.varInt63Len(0x8aL));
+    assertEquals(2, Util.varInt63Len(0x3abcL));
+    assertEquals(3, Util.varInt63Len(0x4abcL));
+    assertEquals(3, Util.varInt63Len(0x1abcdeL));
+    assertEquals(4, Util.varInt63Len(0x2abcdeL));
+    assertEquals(4, Util.varInt63Len(0xfabcdefL));
+    assertEquals(5, Util.varInt63Len(0x1abcdef0L));
+    assertEquals(5, Util.varInt63Len(0x7abcdef01L));
+    assertEquals(6, Util.varInt63Len(0x8abcdef01L));
+    assertEquals(6, Util.varInt63Len(0x3abcdef0123L));
+    assertEquals(7, Util.varInt63Len(0x4abcdef0123L));
+    assertEquals(7, Util.varInt63Len(0x1abcdef012345L));
+    assertEquals(8, Util.varInt63Len(0x2abcdef012345L));
+    assertEquals(8, Util.varInt63Len(0xfabcdef0123456L));
+    assertEquals(9, Util.varInt63Len(0x1abcdef01234567L));
+    assertEquals(9, Util.varInt63Len(0x7abcdef012345678L));
+  }
+
+  @Test
+  public void testVarInt63ReadWrite() {
+    final byte[] data = new byte[9];
+    Util.varInt63Write(0x7abcdef012345678L, data, 0);
+    assertEquals(0x7abcdef012345678L, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0x1abcdef01234567L, data, 0);
+    assertEquals(0x1abcdef01234567L, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0xfabcdef0123456L, data, 0);
+    assertEquals(0xfabcdef0123456L, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0x2abcdef012345L, data, 0);
+    assertEquals(0x2abcdef012345L, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0x1abcdef012345L, data, 0);
+    assertEquals(0x1abcdef012345L, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0x4abcdef0123L, data, 0);
+    assertEquals(0x4abcdef0123L, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0x3abcdef0123L, data, 0);
+    assertEquals(0x3abcdef0123L, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0x8abcdef01L, data, 0);
+    assertEquals(0x8abcdef01L, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0x7abcdef01L, data, 0);
+    assertEquals(0x7abcdef01L, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0x1abcdef0, data, 0);
+    assertEquals(0x1abcdef0L, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0xfabcdefL, data, 0);
+    assertEquals(0xfabcdefL, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0x2abcdeL, data, 0);
+    assertEquals(0x2abcdeL, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0x1abcdeL, data, 0);
+    assertEquals(0x1abcdeL, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0x4abcL, data, 0);
+    assertEquals(0x4abcL, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0x3abcL, data, 0);
+    assertEquals(0x3abcL, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0x8aL, data, 0);
+    assertEquals(0x8aL, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0x7aL, data, 0);
+    assertEquals(0x7aL, Util.varInt63Read(data, 0));
+    Util.varInt63Write(0x0L, data, 0);
+    assertEquals(0x0L, Util.varInt63Read(data, 0));
   }
 }
