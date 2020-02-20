34b418c295f3612931d8186a8c12590e246aec54
kurobako <wyyyrd@gmail.com>
2020-02-20 13:25:34 +0100

    Transducers::cat

--- language/src/test/java/yatta/StdLibTest.java
+++ language/src/test/java/yatta/StdLibTest.java
@@ -3,7 +3,7 @@ package yatta;
 import org.graalvm.polyglot.Value;
 import org.junit.jupiter.api.Test;
 
-import java.util.AbstractList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -34,50 +34,56 @@ public class StdLibTest extends CommonTest {
 
   @Test
   public void sequenceReduceLeftFilterTest() {
-    long ret = context.eval(YattaLanguage.ID, "Seq::reducel [-2,-1,0,1,2] <| Transducers::filter \\val -> val < 0 (0, \\acc val -> acc + val, \\acc -> acc * 2)").asLong();
+    long ret = context.eval(YattaLanguage.ID, "Seq::reducel [-2,-1,0,1,2] <| Transducers::filter \\val -> val < 0 (\\-> 0, \\acc val -> acc + val, \\acc -> acc * 2)").asLong();
     assertEquals(-6L, ret);
   }
 
   @Test
   public void sequenceReduceRightFilterTest() {
-    long ret = context.eval(YattaLanguage.ID, "Seq::reducer [-2,-1,0,1,2] <| Transducers::filter \\val -> val < 0 (0, \\acc val -> acc + val, \\acc -> acc * 2)").asLong();
+    long ret = context.eval(YattaLanguage.ID, "Seq::reducer [-2,-1,0,1,2] <| Transducers::filter \\val -> val < 0 (\\-> 0, \\acc val -> acc + val, \\acc -> acc * 2)").asLong();
     assertEquals(-6L, ret);
   }
 
   @Test
   public void sequenceReduceLeftDropNTest() {
-    long ret = context.eval(YattaLanguage.ID, "Seq::reducel [-2,-1,0,1,2] <| Transducers::drop 2 (0, \\acc val -> acc + val, \\acc -> acc * 2)").asLong();
+    long ret = context.eval(YattaLanguage.ID, "Seq::reducel [-2,-1,0,1,2] <| Transducers::drop 2 (\\-> 0, \\acc val -> acc + val, \\acc -> acc * 2)").asLong();
     assertEquals(6L, ret);
   }
 
   @Test
   public void sequenceReduceLeftTakeNTest() {
-    long ret = context.eval(YattaLanguage.ID, "Seq::reducel [-2,-1,0,1,2] <| Transducers::take 2 (0, \\acc val -> acc + val, \\acc -> acc * 2)").asLong();
+    long ret = context.eval(YattaLanguage.ID, "Seq::reducel [-2,-1,0,1,2] <| Transducers::take 2 (\\-> 0, \\acc val -> acc + val, \\acc -> acc * 2)").asLong();
     assertEquals(-6L, ret);
   }
 
   @Test
   public void sequenceReduceLeftDedupeTest() {
-    long ret = context.eval(YattaLanguage.ID, "Seq::reducel [1, 1, 2, 3, 3, 4] <| Transducers::dedupe (0, \\acc val -> acc + val, \\acc -> acc * 2)").asLong();
+    long ret = context.eval(YattaLanguage.ID, "Seq::reducel [1, 1, 2, 3, 3, 4] <| Transducers::dedupe (\\-> 0, \\acc val -> acc + val, \\acc -> acc * 2)").asLong();
     assertEquals(20L, ret);
   }
 
   @Test
   public void sequenceReduceLeftDistinctTest() {
-    long ret = context.eval(YattaLanguage.ID, "Seq::reducel [1, 2, 3, 4, 1, 2, 3, 4] <| Transducers::distinct (0, \\acc val -> acc + val, \\acc -> acc * 2)").asLong();
+    long ret = context.eval(YattaLanguage.ID, "Seq::reducel [1, 2, 3, 4, 1, 2, 3, 4] <| Transducers::distinct (\\-> 0, \\acc val -> acc + val, \\acc -> acc * 2)").asLong();
     assertEquals(20L, ret);
   }
 
   @Test
   public void sequenceReduceLeftChunkTest() {
-    long ret = context.eval(YattaLanguage.ID, "Seq::reducel [6, 1, 5, 2, 4, 3] <| Transducers::chunk 2 (1, \\acc val -> acc * Seq::len val, \\acc -> acc)").asLong();
+    long ret = context.eval(YattaLanguage.ID, "Seq::reducel [6, 1, 5, 2, 4, 3] <| Transducers::chunk 2 (\\-> 1, \\acc val -> acc * Seq::len val, identity)").asLong();
     assertEquals(8L, ret);
   }
 
   @Test
   public void sequenceReduceLeftScanTest() {
-    long ret = context.eval(YattaLanguage.ID, "Seq::reducel [1, 2, 3] <| Transducers::scan (0, \\ acc val -> acc + Seq::len val, \\acc -> acc)").asLong();
+    long ret = context.eval(YattaLanguage.ID, "Seq::reducel [1, 2, 3] <| Transducers::scan (\\-> 0, \\ acc val -> acc + Seq::len val, identity)").asLong();
     assertEquals(6L, ret);
+  }
+
+  @Test
+  public void sequenceReduceLeftCatTest() {
+    long ret = context.eval(YattaLanguage.ID, "let reduce_set = Set::reduce in Seq::reducel [{1, 2}, {3, 4}] <| Transducers::cat reduce_set (\\-> 0, \\acc val -> acc + val, identity)").asLong();
+    assertEquals(10L, ret);
   }
 
   @Test
@@ -88,7 +94,7 @@ public class StdLibTest extends CommonTest {
 
   @Test
   public void dictReduceMapTest() {
-    long ret = context.eval(YattaLanguage.ID, "Dict::reduce {'a' = 1, 'b' = 2, 'c' = 3} <| Transducers::map \\val -> val (0, \\ state val -> state + 1, \\state -> state * 2)").asLong();
+    long ret = context.eval(YattaLanguage.ID, "Dict::reduce {'a' = 1, 'b' = 2, 'c' = 3} <| Transducers::map \\val -> val (\\-> 0, \\ state val -> state + 1, \\state -> state * 2)").asLong();
     assertEquals(6L, ret);
   }
 
@@ -100,13 +106,13 @@ public class StdLibTest extends CommonTest {
 
   @Test
   public void setReduceFilterTest() {
-    long ret = context.eval(YattaLanguage.ID, "Set::reduce {-2,-1,0,1,2} <| Transducers::filter \\val -> val < 0 (0, \\state val -> state + val, \\state -> state * 2)").asLong();
+    long ret = context.eval(YattaLanguage.ID, "Set::reduce {-2,-1,0,1,2} <| Transducers::filter \\val -> val < 0 (\\-> 0, \\state val -> state + val, \\state -> state * 2)").asLong();
     assertEquals(-6L, ret);
   }
 
   @Test
   public void setReduceMapTest() {
-    long ret = context.eval(YattaLanguage.ID, "Set::reduce {1, 2, 3} <| Transducers::map \\val -> val + 1 (0, \\ state val -> state + val, \\state -> state * 2)").asLong();
+    long ret = context.eval(YattaLanguage.ID, "Set::reduce {1, 2, 3} <| Transducers::map \\val -> val + 1 (\\-> 0, \\ state val -> state + val, \\state -> state * 2)").asLong();
     assertEquals(18L, ret);
   }
 
@@ -117,7 +123,7 @@ public class StdLibTest extends CommonTest {
 
     Object[] array = tuple.as(Object[].class);
     assertEquals(0L, array[0]);
-    assertEquals("ahoj", ((AbstractList) array[1]).get(0));
+    assertEquals("ahoj", ((List) array[1]).get(0));
     assertTrue(((String) array[2]).isEmpty()); // empty Seq will always evaluate isString to 0 and then polyglot will return empty string (even though technically it should be an empty Seq of Seqs(strings, lines))
   }
 }
