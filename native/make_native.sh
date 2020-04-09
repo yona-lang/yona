e19dc596640a98996cfb07fd270994c98b7d3e24
Adam Kovari <kovariadam@gmail.com>
2020-04-09 10:46:58 +0200

    attempt to fix build in travis

--- native/make_native.sh
+++ native/make_native.sh
@@ -3,8 +3,8 @@ if [[ $YATTA_BUILD_NATIVE == "false" ]]; then
     echo "Skipping the native image build because YATTA_BUILD_NATIVE is set to false."
     exit 0
 fi
-"$JAVA_HOME"/bin/native-image --macro:truffle -H:MaxRuntimeCompileMethods=1200 \
-    --initialize-at-build-time=yatta \
+"$JAVA_HOME"/bin/native-image \
+    --macro:truffle --no-fallback --initialize-at-build-time \
     -cp ../language/target/yatta.jar:../launcher/target/launcher-0.1-SNAPSHOT.jar \
     yatta.Launcher \
     yattanative
