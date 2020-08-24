cd5bdabac7431787dbeac0cf7159f228bedf588c
Adam Kovari <kovariadam@gmail.com>
2020-08-24 20:55:40 +0200

    Creating a new "native" profile for building the yonanative component. Disabled by default

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM oracle/graalvm-ce:20.1.0-java11
 MAINTAINER Adam Kovari <kovariadam@gmail.com>
 
 RUN yum install -y git
-RUN gu install native-image
+#RUN gu install native-image
 
 ARG MAVEN_VERSION=3.6.3
 ARG SHA=c35a1803a6e70a126e80b2b3ae33eed961f83ed74d18fcd16909b2d44d7dada3203f1ffe726c17ef8dcca2dcaa9fca676987befeadc9b9f759967a8cb77181c0
--- component/make_component.sh
+++ component/make_component.sh
@@ -4,7 +4,7 @@ readonly COMPONENT_DIR="component_temp_dir"
 readonly LANGUAGE_PATH="$COMPONENT_DIR/languages/yona"
 readonly CURRENT_TAG=$(git describe --tags --exact-match || echo "SNAPSHOT")
 if [[ -f ../native/yonanative ]]; then
-    INCLUDE_YONANATIVE="TRUE"
+  INCLUDE_YONANATIVE="TRUE"
 fi
 
 rm -rf COMPONENT_DIR
@@ -17,8 +17,8 @@ cp ../launcher/target/yona-launcher.jar "$LANGUAGE_PATH/launcher/"
 
 mkdir -p "$LANGUAGE_PATH/bin"
 cp ../yona $LANGUAGE_PATH/bin/
-if [[ $INCLUDE_YONANATIVE -eq "TRUE" ]]; then
-    cp ../native/yonanative $LANGUAGE_PATH/bin/
+if [[ $INCLUDE_YONANATIVE == "TRUE" ]]; then
+  cp ../native/yonanative $LANGUAGE_PATH/bin/
 fi
 
 cp -R ../language/lib-yona "$LANGUAGE_PATH"
@@ -30,31 +30,32 @@ touch "$LANGUAGE_PATH/native-image.properties"
   echo "Requires = language:regex"
   echo "JavaArgs = -Xmx3G \\"
   echo "           -Dpolyglot.image-build-time.PreinitializeContexts=yona"
-} > "$LANGUAGE_PATH/native-image.properties"
+} >"$LANGUAGE_PATH/native-image.properties"
 
 mkdir -p "$COMPONENT_DIR/META-INF"
 {
-    echo "Bundle-Name: yona";
-    echo "Bundle-Symbolic-Name: yona";
-    echo "Bundle-Version: 0.8.1-$CURRENT_TAG";
-    echo 'Bundle-RequireCapability: org.graalvm; filter:="(&(graalvm_version=20.1.0)(os_arch=amd64))"';
-    echo "x-GraalVM-Polyglot-Part: True"
-} > "$COMPONENT_DIR/META-INF/MANIFEST.MF"
+  echo "Bundle-Name: yona"
+  echo "Bundle-Symbolic-Name: yona"
+  echo "Bundle-Version: 0.8.1-$CURRENT_TAG"
+  echo 'Bundle-RequireCapability: org.graalvm; filter:="(&(graalvm_version=20.1.0)(os_arch=amd64))"'
+  echo "x-GraalVM-Polyglot-Part: True"
+} >"$COMPONENT_DIR/META-INF/MANIFEST.MF"
 
 (
-cd $COMPONENT_DIR || exit 1
-jar cfm ../yona-component.jar META-INF/MANIFEST.MF .
+  cd $COMPONENT_DIR || exit 1
+  jar cfm ../yona-component.jar META-INF/MANIFEST.MF .
 
-echo "bin/yona = ../languages/yona/bin/yona" > META-INF/symlinks
-if [[ $INCLUDE_YONANATIVE -eq "TRUE" ]]; then
-    echo "bin/yonanative = ../languages/yona/bin/yonanative" >> META-INF/symlinks
-fi
-jar uf ../yona-component.jar META-INF/symlinks
+  echo "bin/yona = ../languages/yona/bin/yona" >META-INF/symlinks
+  if [[ $INCLUDE_YONANATIVE == "TRUE" ]]; then
+    echo "bin/yonanative = ../languages/yona/bin/yonanative" >>META-INF/symlinks
+  fi
+  jar uf ../yona-component.jar META-INF/symlinks
 
-{
-    echo "languages/yona/bin/yona = rwxrwxr-x"
-    echo "languages/yona/bin/yonanative = rwxrwxr-x"
-} > META-INF/permissions
-jar uf ../yona-component.jar META-INF/permissions
+  echo "languages/yona/bin/yona = rwxrwxr-x" >META-INF/permissions
+  if [[ $INCLUDE_YONANATIVE == "TRUE" ]]; then
+    echo "languages/yona/bin/yonanative = rwxrwxr-x" >META-INF/permissions
+  fi
+
+  jar uf ../yona-component.jar META-INF/permissions
 )
 rm -rf $COMPONENT_DIR
